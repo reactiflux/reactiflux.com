6e87d9252e6c38756bfef063263340b4c29bf555
Zachary Gibson <zach.gibson@buzznog.com>
2016-11-22 09:29:39 -0600

    Add animation/transition to link hover

--- utils/components.js
+++ utils/components.js
@@ -25,10 +25,13 @@ export const Logo = styled(Link)`
 
 export const StyledLink = styled(Link)`
   text-decoration: none;
-  padding-bottom: 4px;
+  padding-bottom: 6px;
   margin-right: 40px;
+  border-bottom: solid 4px rgba(255, 255, 255, 0);
+  transition: all 175ms cubic-bezier(0.65, 0.05, 0.36, 1);
   &:hover {
-    border-bottom: 2px solid #dd1d64;
+    padding-bottom: 4px;
+    border-bottom: 4px solid #dd1d64;
   }
   &:last-child {
     margin-right: 0px;
