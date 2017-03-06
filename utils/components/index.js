50bbede8db11abf9663af074f836658a7d8ee03f
Fabio Salimbeni <fabio.salimbeni@gmail.com>
2017-03-06 01:37:20 +0000

    Split styled components

---
+++ utils/components/index.js
@@ -0,0 +1,68 @@
+import {
+    NavigationLink,
+    LinkList,
+    Buttons,
+    Button,
+    MovedLink,
+    IconLink,
+    Toggle,
+    StyledLink,
+    SocialLinks,
+    Arrow
+} from './links/'
+import {
+    Header,
+    Navigation,
+    SideBarInner,
+    FrontPage,
+    Title,
+    Subtitle,
+    Footer,
+    Container,
+    MarkdownContainer,
+    SideBar,
+    TitleBox,
+    SmallTitle,
+    Text
+} from './structure/'
+import {
+    ReadMore,
+    Copyright,
+    Credits,
+    PublishDate,
+    Transcript,
+    Logo
+} from './misc'
+
+export default {
+     NavigationLink,
+    LinkList,
+    Buttons,
+    Button,
+    MovedLink,
+    IconLink,
+    Toggle,
+    StyledLink,
+    SocialLinks,
+    Arrow,
+    Header,
+    Navigation,
+    SideBarInner,
+    FrontPage,
+    Title,
+    Subtitle,
+    Footer,
+    Container,
+    MarkdownContainer,
+    SideBar,
+    TitleBox,
+    SmallTitle,
+    Text,
+    ReadMore,
+    Copyright,
+    Credits,
+    PublishDate,
+    Transcript,
+    Logo
+}
+
