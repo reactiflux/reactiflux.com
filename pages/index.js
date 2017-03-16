7bc4f9fb6082a59aa9948549aa3b586ca243065b
Kyle Mathews <mathews.kyle@gmail.com>
2017-03-15 23:19:38 -0700

    Improve general meta description for site

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default class Index extends React.Component {
         <Helmet
           title={config.siteTitle}
           meta={[
-            {"name": "description", "content": "Reactiflux webpage"}
+            {"name": "description", "content": "We’re a chat community of 20,000+ React JS, React Native, Redux, Relay and GraphQL developers."}
           ]}
         />
         <TitleBox>
