999c4bbea981423b999b5977b1a6421fbb60ae47
Carl Vitullo <carl.vitullo@gmail.com>
2019-11-02 00:52:23 -0400

    Nearly working transcripts

---
+++ src/pages/transcripts.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Helmet from "react-helmet";
+import {
+  Container,
+  SmallTitle,
+  SideBar,
+  Transcript,
+  StyledLink
+} from "../utils/components";
+import NavBar from "../utils/components/NavBar";
+
+export default function Transcripts({ data }) {
+  const articles = data.transcripts.nodes.map(node => {
+    return {
+      title: node.childMarkdownRemark.frontmatter.title,
+      path: `/${node.relativeDirectory}/${node.name}`,
+      html: node.childMarkdownRemark.html
+    };
+  });
+
+  const newestArticle = articles[0];
+
+  return (
+    <Container>
+      <NavBar />
+      <Helmet title={"Reactiflux transcripts"} />
+      <SmallTitle>{newestArticle.title}</SmallTitle>
+      <SideBar>
+        {articles.map(article => (
+          <li key={article.title}>
+            <StyledLink
+              to={"/transcripts/" + article.path}
+              title={article.title}
+            >
+              {article.title}
+            </StyledLink>
+          </li>
+        ))}
+      </SideBar>
+      <Transcript article={newestArticle} />
+    </Container>
+  );
+}
+
+export const pageQuery = graphql`
+  query AllTranscripts {
+    transcripts: allFile(
+      filter: { sourceInstanceName: { eq: "transcripts" } }
+      sort: { fields: childMarkdownRemark___frontmatter___date, order: DESC }
+    ) {
+      nodes {
+        name
+        relativeDirectory
+        childMarkdownRemark {
+          html
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`;
---
+++ src/utils/components/NavBar.js
@@ -0,0 +1,98 @@
+import React from "react";
+import styled from "styled-components";
+import Headroom from "react-headroom";
+import Styles from "../../css/styles";
+
+// Add our typefaces.
+import "typeface-poppins";
+import "typeface-work-sans";
+import "typeface-space-mono";
+
+import { Header, Navigation, Toggle, Logo, NavigationLink } from "./index";
+
+const HeaderEl = styled(Header)`
+  width: 100%;
+`;
+
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: false
+    };
+  }
+
+  onMobile = () => window.innerWidth <= 768;
+
+  toggle = item => () => {
+    this.setState(prevState => ({
+      [item]: !prevState[item]
+    }));
+  };
+
+  close = item => () => {
+    if (this.onMobile()) {
+      this.setState({
+        [item]: false
+      });
+    }
+  };
+
+  render() {
+    const { menu } = this.state;
+    const closeMenu = this.close("menu");
+
+    return (
+      <Headroom style={{ position: "fixed" }} disableInlineStyles>
+        <Styles />
+        <HeaderEl>
+          <Logo to="/" onClick={closeMenu}>
+            Reactiflux
+          </Logo>
+          <Navigation role="navigation" style={{ top: menu ? 0 : "-100vh" }}>
+            <NavigationLink to="/tips/" title="Tips" onClick={closeMenu}>
+              Tips
+            </NavigationLink>
+            <NavigationLink
+              to="/guidelines/"
+              title="Community Guidelines"
+              onClick={closeMenu}
+            >
+              Community Guidelines
+            </NavigationLink>
+            <NavigationLink
+              to="/schedule/"
+              title="Q&A Schedule"
+              onClick={closeMenu}
+            >
+              Q&A Schedule
+            </NavigationLink>
+            <NavigationLink
+              to="/transcripts/"
+              title="Transcripts"
+              onClick={closeMenu}
+            >
+              Transcripts
+            </NavigationLink>
+            <NavigationLink
+              to="http://jobs.reactiflux.com"
+              title="Jobs"
+              target="_blank"
+              onClick={closeMenu}
+            >
+              Jobs
+            </NavigationLink>
+            <NavigationLink
+              to="/learning/"
+              title="Learning"
+              onClick={closeMenu}
+            >
+              Learning
+            </NavigationLink>
+          </Navigation>
+          <Toggle onClick={this.toggle("menu")} active={menu} />
+        </HeaderEl>
+      </Headroom>
+    );
+  }
+}
