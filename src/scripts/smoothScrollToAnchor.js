e71087578db672ba11ae3afaf52666549bc8f9a6
Joe McGrath <joe@raptori.dev>
2019-11-23 19:45:39 +0000

    fix anchor links in transcripts

--- src/scripts/smoothScrollToAnchor.js
+++ src/scripts/smoothScrollToAnchor.js
@@ -1,11 +1,17 @@
-;(function () {
-    document.addEventListener('click', function (e) {
-        if (e.target.matches('a[href^="#"]')) {
-            e.preventDefault()
+;(function() {
+  document.addEventListener('click', function(e) {
+    if (e.target.matches('a[href^="#"]')) {
+      e.preventDefault();
 
-            document.querySelector(e.target.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
-    })
-})()
+      const target = e.target.getAttribute('href').replace('#', '');
+
+      const element =
+        document.getElementById(target) ||
+        document.querySelector(`[name="${target}"]`);
+
+      element.scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
+  });
+})();
