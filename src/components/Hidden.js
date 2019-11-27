876671e9f8a99088e96e23b51f4e395f45ec3acf
Joe McGrath <joe@raptori.dev>
2019-11-27 21:01:24 +0000

    run prettier

--- src/components/Hidden.js
+++ src/components/Hidden.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const Hidden = styled.div`
-    clip: rect(1px, 1px, 1px, 1px);
-    height: 1px;
-    overflow: hidden;
-    position: absolute !important;
-    white-space: nowrap;
-    width: 1px;
-`
+  clip: rect(1px, 1px, 1px, 1px);
+  height: 1px;
+  overflow: hidden;
+  position: absolute !important;
+  white-space: nowrap;
+  width: 1px;
+`;
