b985accbe7b6e1ad212bb7006fca209c1a5ed48d
Carl Vitullo <carl.vitullo@gmail.com>
2021-12-18 18:00:38 -0500

    Working transcripts page

---
+++ src/pages/transcripts.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { compareDesc, parseISO } from "date-fns";
+
+import { FocusBoundary, Layout, Link } from "@components";
+import { loadAllMd, mdToHtml } from "@helpers/retrieveMdPages";
+
+export default function Transcripts({
+  all,
+  latest,
+}: Awaited<ReturnType<typeof getStaticProps>>["props"]) {
+  return (
+    <Layout title="Transcripts" sidebar as={undefined} description={undefined}>
+      {(setSidebar: any) => (
+        <>
+          <h1>{latest.title}</h1>
+          <FocusBoundary
+            onChange={setSidebar}
+            onEnter={undefined}
+            onExit={undefined}
+          >
+            <nav>
+              <ol>
+                {all.map((transcript) => (
+                  <li key={transcript.path}>
+                    <Link href={transcript.path}>{transcript.title}</Link>
+                  </li>
+                ))}
+              </ol>
+            </nav>
+          </FocusBoundary>
+          <div>
+            <time>Transcript from {latest.date}</time>
+            <div dangerouslySetInnerHTML={{ __html: latest.html }} />
+          </div>
+        </>
+      )}
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const transcripts = await loadAllMd("src/transcripts");
+  const all = transcripts
+    .filter(Boolean)
+    .sort((a, b) =>
+      a.date && b.date ? compareDesc(parseISO(a.date), parseISO(b.date)) : 1,
+    )
+    .map((t) => ({
+      ...t,
+      path: `/transcripts/${t.slug}`,
+    }));
+  // Find first (most recent) transcript with content and generate html
+  const latest = all.find((t) => t.content !== "");
+  if (!latest) {
+    throw new Error("No transcripts found!");
+  }
+  return {
+    props: {
+      all,
+      latest: { ...latest, html: mdToHtml(latest.content) },
+    },
+  };
+}
