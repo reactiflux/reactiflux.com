b3bfe8b6c66d6e643b378e69e7a6ab501ed983cf
Carl Vitullo <carl.vitullo@gmail.com>
2021-12-20 18:04:55 -0500

    Move object/array helpers

--- src/helpers/object.ts
+++ src/helpers/object.ts
@@ -2,9 +2,9 @@ export const pick = <T extends Record<string, any>, K extends keyof T>(
   keys: K[],
   data: T,
 ) => {
-  const out = {} as Pick<T, K>;
+  const out = {} as any;
   for (let key of keys) {
-    out[key] = data[key];
+    out[key] = typeof data[key] === "undefined" ? null : data[key];
   }
-  return out;
+  return out as Pick<T, K>;
 };
