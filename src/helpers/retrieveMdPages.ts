ef008dd5a3db474817f7d62bfbe23964fe088a2f
Carl Vitullo <carl.vitullo@gmail.com>
2021-12-22 15:57:40 -0500

    Add @vcarl/remark-headings and unpack remark/rehype/unified

--- src/helpers/retrieveMdPages.ts
+++ src/helpers/retrieveMdPages.ts
@@ -2,8 +2,13 @@ import fs from "fs/promises";
 import { join } from "path";
 import { format } from "date-fns";
 import matter from "gray-matter";
-import remark from "remark";
-import remarkHtml from "remark-html";
+import { unified } from "unified";
+import parse from "remark-parse";
+import stringify from "remark-stringify";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypeSlug from "rehype-slug";
+import remarkHeadings, { hasHeadingsData } from "@vcarl/remark-headings";
 
 const loadMd = async (path: string) => {
   const fullPath = join(process.cwd(), `${path}.md`);
@@ -14,9 +19,21 @@ const loadMd = async (path: string) => {
 const stripSuffix = (filename: string, suffix: string) =>
   filename.replace(new RegExp(`\.${suffix}`), "");
 
-const remarkHtmlProcessor = remark().use(remarkHtml);
-export const mdToHtml = (mdSource: string) =>
-  remarkHtmlProcessor.processSync(mdSource).toString();
+const remarkHtmlProcessor = unified()
+  .use(parse)
+  .use(stringify)
+  .use(remarkRehype)
+  .use(remarkHeadings)
+  .use(rehypeSlug)
+  .use(rehypeStringify);
+
+export const processMd = (mdSource: string) => {
+  const vfile = remarkHtmlProcessor.processSync(mdSource);
+  if (hasHeadingsData(vfile.data)) {
+    return { html: vfile.toString(), headings: vfile.data.headings };
+  }
+  return { html: vfile.toString(), headings: [] };
+};
 
 export const loadAllMd = async <Frontmatter>(directory: string) => {
   const postsDirectory = join(process.cwd(), directory);
@@ -58,10 +75,6 @@ export const loadMdBySlug = async <Frontmatter>(
     // It's really annoying that the parser doesn't let us do this
     if (pair[1] instanceof Date) {
       return [pair[0], format(pair[1], "yyyy-MM-dd")];
-    }
-    // Parse the description, since it's always pretty small
-    if (pair[0] === "description" || pair[0] === "people") {
-      return [pair[0], mdToHtml(pair[1])];
     }
     return pair;
   });
