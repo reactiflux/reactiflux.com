c013a86df2eaaf4c7eb9457d2c01927ddd42430f
Nick McCurdy <nick@nickmccurdy.com>
2022-05-27 02:11:00 -0400

    Reinstall Google Analytics (#228)
    * Reinstall Google Analytics
    
    * Only track with Google Analytics 4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,34 @@
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
+
+const GA_TRACKING_ID = "G-HYLJ4BYC6Z";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              dataLayer.push(arguments);
+            }
+            gtag("js", new Date());
+            gtag("config", "${GA_TRACKING_ID}");
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
