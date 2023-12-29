ec29355b420ad144f9bab2907d35cd7877350354
Carl Vitullo <carl.vitullo@gmail.com>
2023-12-29 16:29:12 -0500

    Remove indiebound

--- src/pages/learning/books.tsx
+++ src/pages/learning/books.tsx
@@ -51,7 +51,6 @@ interface RawBookProps {
   title?: string;
   alt?: string;
   onlineLink?: string;
-  localLink?: string;
 }
 
 const ratio = 166 / 250;
@@ -65,7 +64,6 @@ const RawBook = ({
   title = "",
   alt = "",
   onlineLink = "",
-  localLink = "",
 }: RawBookProps) => (
   <Li>
     <Figure>
@@ -87,7 +85,6 @@ const RawBook = ({
       <Caption>
         {title}
         {onlineLink && <BuyLink href={onlineLink}>Buy online</BuyLink>}
-        {localLink && <BuyLink href={localLink}>Buy locally</BuyLink>}
       </Caption>
     </Figure>
   </Li>
@@ -118,7 +115,6 @@ const Book = ({
     cover={cover || `https://covers.openlibrary.org/b/isbn/${isbn}.jpg`}
     title={title}
     onlineLink={`https://bookshop.org/a/88607/${isbn}`}
-    localLink={`https://www.indiebound.org/book/${isbn}?aff=reactiflux`}
   />
 );
 
