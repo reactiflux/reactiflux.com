9386c341b3eab372f57a3140df6c47af22a4e029
Nick McCurdy <nick@nickmccurdy.com>
2024-02-02 10:30:53 -0500

    Use Next's GoogleAnalytics component (#287)
    * Use Next's GoogleAnalytics component
    
    * Unpin @next/third-parties range for easier updates
    
    * Upgrade @next/google-analytics from canary to latest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,36 +1,16 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
-import Script from "next/script";
 
 import "minireset.css";
 import "typeface-poppins";
 import "typeface-work-sans";
 import "typeface-space-mono";
 
-const GA_TRACKING_ID = "G-HYLJ4BYC6Z";
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-              dataLayer.push(arguments);
-            }
-            gtag("js", new Date());
-            gtag("config", "${GA_TRACKING_ID}");
-          `,
-        }}
-      />
+      <GoogleAnalytics gaId="G-HYLJ4BYC6Z" />
       <Component {...pageProps} />
     </>
   );
