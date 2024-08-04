aba978e4904869bf438919b0d8dc6210ccd84632
barrybtw <nicolai.bernhard03@gmail.com>
2024-08-04 02:23:00 +0200

    adding layout to podcast page and todo messages

--- src/pages/podcasts/[slug].tsx
+++ src/pages/podcasts/[slug].tsx
@@ -1,26 +1,44 @@
 import { useEffect, useRef, useState } from "react";
 import { z } from "zod";
 import { type RootTranscript } from "@utils/transcript-types";
+import { useRouter } from "next/router";
+import { Layout } from "@components";
 
-// TODO For this page, make it possible to use the [slug] to grab a m4a file, and a transcript file for it to work with other podcasts.
-
-const getTranscript = (slug: Slug) => {
-  return fetch("/podcastassets/2024-06.json").then((response) =>
-    response.json(),
+// TODO: Fix the links to actually point to the real place
+const getTranscript = async (slug: Slug) => {
+  // const resp = await fetch(`https://something.com/${slug}.json`);
+  const resp = await fetch(
+    "https://d1d3n03t5zntha.cloudfront.net/106d7fc0-ae1b-4b25-90c6-490187bb4b2c/document-42b597473660461ab29e22ee964133e6.json",
   );
+  const isJson = z.any(z.instanceof(File)).safeParse(resp);
+  if (resp.ok && isJson.success) return await isJson.data.json();
+  return { error: resp.status, valid: isJson.success };
 };
 
-const getAudio = (slug: Slug) => {
-  return fetch(`/podcastassets/2024-06.m4a`).then((response) =>
-    response.blob(),
-  );
+const getAudio = async (slug: Slug) => {
+  // const resp = await fetch(`https://something.com/${slug}.m4a`);
+  const resp = await fetch(`/original.m4a`);
+
+  if (resp.ok) return await resp.blob();
 };
 
 const m4a = z.any(z.instanceof(File));
 
 type Slug = `${number}-${number}`;
 
-export default function PodPage() {
+export const getStaticProps = (t: { params: { slug: Slug } }) => {
+  console.log(t);
+
+  return {
+    props: {
+      slug: t.params.slug,
+    },
+  };
+};
+
+export default function PodPage({ slug }: { slug: Slug }) {
+  console.log(slug);
+
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const [currentTime, setCurrentTime] = useState(0);
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -37,6 +55,8 @@ export default function PodPage() {
     { id: string; location: number; text: string }[]
   >([]);
 
+  const router = useRouter();
+
   const paragraphRefs = useRef<(HTMLParagraphElement | null)[]>([]);
   const wordRefs = useRef<(HTMLSpanElement | null)[]>([]);
   const lastClickedTimeRef = useRef<number | null>(null);
@@ -47,7 +67,7 @@ export default function PodPage() {
         const currentAudioTime = audioRef.current.currentTime;
         setCurrentTime(currentAudioTime);
       }
-    }, 250);
+    }, 175);
     return () => clearInterval(interval);
   }, []);
 
@@ -75,8 +95,8 @@ export default function PodPage() {
   useEffect(() => {
     (async () => {
       const [transcriptFetch, audioFetch] = await Promise.allSettled([
-        getTranscript("24-06"),
-        getAudio("24-06"),
+        getTranscript(slug),
+        getAudio(slug),
       ]);
 
       if (
@@ -110,6 +130,13 @@ export default function PodPage() {
         setMarkers(markerData);
 
         const audioBlob = audioFetch.value;
+        console.log(audioBlob);
+
+        if (audioBlob == undefined) {
+          setLoading(false);
+          router.push("/");
+          return;
+        }
         const audioUrl = URL.createObjectURL(audioBlob);
         setAudioSrc(audioUrl);
 
@@ -190,7 +217,6 @@ export default function PodPage() {
 
     const paragraphElements = [];
 
-    // Add speaker name if it changes
     if (currentSpeakerId && currentSpeakerId !== lastSpeakerId) {
       paragraphElements.push(
         <div
@@ -207,7 +233,6 @@ export default function PodPage() {
       lastSpeakerId = currentSpeakerId;
     }
 
-    // Add markers before the paragraph if there are any
     markers.forEach((marker) => {
       if (marker.location <= wordCounter) {
         paragraphElements.push(
@@ -216,7 +241,7 @@ export default function PodPage() {
             style={{
               marginTop: "1rem",
               marginBottom: "1rem",
-              backgroundColor: "#FFD700", // Marker color
+              backgroundColor: "#FFD700",
               padding: "0.5rem",
               borderRadius: "5px",
             }}
@@ -224,7 +249,7 @@ export default function PodPage() {
             {marker.text}
           </div>,
         );
-        markers.splice(markers.indexOf(marker), 1); // Remove the marker after adding it
+        markers.splice(markers.indexOf(marker), 1);
       }
     });
 
@@ -280,60 +305,75 @@ export default function PodPage() {
   }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        margin: "auto",
-        padding: "20px",
-        backgroundColor: "#f9f9f9",
-      }}
+    <Layout
+      title={`Podcast: ${slug}`}
+      description={"Audio podcast with transcript"}
+      as={undefined}
+      sidebar
     >
       <div
         style={{
-          maxWidth: "800px",
-          width: "100%",
-          backgroundColor: "#fff",
-          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-          borderRadius: "8px",
-          padding: "20px",
-          lineHeight: "1.6",
-          fontFamily: "'Roboto', sans-serif",
-          color: "#333",
-          marginBottom: "100px", // Adjusted for the audio player height and spacing
-        }}
-      >
-        {paragraphs}
-      </div>
-
-      <div
-        style={{
-          position: "fixed",
-          bottom: "20px",
-          left: "50%",
-          transform: "translateX(-50%)",
-          width: "90%",
-          maxWidth: "600px",
-          backgroundColor: "#fff",
-          boxShadow: "0 4px 16px rgba(0, 0, 0, 0.2)",
-          borderRadius: "16px",
-          padding: "10px 20px",
           display: "flex",
+          flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          zIndex: 1000,
-          border: "2px solid #ececec",
+          margin: "auto",
+          padding: "20px",
+          backgroundColor: "#f9f9f9",
         }}
       >
-        <audio
-          ref={audioRef}
-          controls
-          src={audioSrc as string}
-          style={{ width: "100%" }}
-        ></audio>
+        <div
+          style={{
+            maxWidth: "800px",
+            width: "100%",
+            backgroundColor: "#fff",
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+            borderRadius: "8px",
+            padding: "20px",
+            lineHeight: "1.6",
+            fontFamily: "'Roboto', sans-serif",
+            color: "#333",
+            marginBottom: "100px",
+          }}
+        >
+          {paragraphs}
+        </div>
+
+        <div
+          style={{
+            position: "fixed",
+            bottom: "20px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            width: "90%",
+            maxWidth: "600px",
+            backgroundColor: "#fff",
+            boxShadow: "0 4px 16px rgba(0, 0, 0, 0.2)",
+            borderRadius: "16px",
+            padding: "10px 20px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            zIndex: 1000,
+            border: "2px solid #ececec",
+          }}
+        >
+          <audio
+            ref={audioRef}
+            controls
+            src={audioSrc as string}
+            style={{ width: "100%" }}
+          ></audio>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
+
+export const getStaticPaths = async () => {
+  const slugs = [{ slug: "24-06" }]; // TODO: Dynamically get available slugs/podcasts
+  return {
+    paths: slugs.map(({ slug }) => ({ params: { slug } })),
+    fallback: false,
+  };
+};
