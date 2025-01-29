5affc53cfe6ff830b9dd1fa3dd41e31f40eed07d
Carl Vitullo <carl.vitullo@gmail.com>
2025-01-28 19:30:43 -0500

    Update job board page to sync with realtime data

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -3,92 +3,113 @@ import moment from "moment";
 import styled, { css } from "styled-components";
 import { transparentize } from "polished";
 
-import { Link, Tag } from "@components";
-import { addLinks, extractTags } from "@utils/string";
-import { pink } from "@utils/theme";
+import { Link, Tag, Reaction } from "@components";
+import { darkPink, pink } from "@utils/theme";
 
-const Wrapper = styled.div`
-  ${(props) =>
-    props.last
-      ? ""
-      : css`
-          border-bottom: 2px solid ${pink};
-          margin-bottom: 4rem;
-          padding-bottom: 2rem;
-        `}
-
-  div {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+const Wrapper = styled.article`
+  &:not(:last-of-type) {
+    border-bottom: 2px solid ${pink};
+    margin-bottom: 2.5rem;
+  }
+  p {
+    margin-bottom: 0;
   }
 `;
-
-const Pre = styled.pre`
-  font-family: inherit;
-  white-space: pre-wrap;
+const PostBody = styled.div`
+  margin-bottom: 1.75rem;
+`;
+const Tags = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 0.5rem;
+`;
+const Reactions = styled(Tags)`
+  padding-bottom: 2rem;
 `;
 
-const GetInTouch = styled.p`
-  flex-grow: 1;
-  margin-bottom: 0;
-  padding-left: 1rem;
-  text-align: right;
-
-  button {
-    background: none;
-    border: none;
-    color: ${(props) => transparentize(0.5, pink(props))};
-    cursor: pointer;
-    font-size: inherit;
-    font-family: inherit;
-    padding: 0;
-  }
+const GetInTouch = styled.button`
+  background: none;
+  border: 2px solid ${pink};
+  color: ${pink};
+  margin-left: 1rem;
+  border-radius: 100%;
+  padding: 0 0.35rem;
+  cursor: pointer;
 `;
 
-const getJobLink = (id) =>
-  `https://discord.com/channels/102860784329052160/103882387330457600/${id}`;
+const PostAuthor = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const AuthorInfo = styled.div`
+  display: inline-block;
+  padding: 0.75rem 0;
+`;
+const Avatar = styled.img`
+  display: inline-block;
+  width: 3rem;
+  height: 3rem;
+  margin-right: 0.25rem;
+  border-radius: 100%;
+`;
+
 const getAuthorLink = (id) => `https://discord.com/users/${id}`;
+
+const Author = ({
+  authorHref,
+  messageHref,
+  name,
+  authorImageSrc,
+  postTime,
+  onInfoClick,
+}) => (
+  <PostAuthor>
+    <Avatar src={authorImageSrc} />
+    <AuthorInfo>
+      <Link href={authorHref}>
+        <strong>{name}</strong>
+      </Link>{" "}
+      on{" "}
+      <Link href={messageHref}>{moment(postTime).format("MMMM Do YYYY")}</Link>
+    </AuthorInfo>
+    <GetInTouch onClick={onInfoClick}>ℹ</GetInTouch>
+  </PostAuthor>
+);
 
 export const Offer = ({
   author,
-  date,
-  id,
-  last,
-  message,
+  messageLink,
+  createdAt,
+  reactions,
+  tags,
+  description,
   onClickGetInTouch,
 }) => {
-  const [tags, content] = extractTags(message);
   return (
-    <Wrapper last={last}>
-      <p>
+    <Wrapper>
+      <Author
+        authorHref={getAuthorLink(author.id)}
+        name={author.displayName}
+        messageHref={messageLink}
+        authorImageSrc={author.avatar}
+        postTime={createdAt}
+        onInfoClick={onClickGetInTouch}
+      />
+      <Tags>
         {tags.map((tag) => (
           <Tag key={tag}>{tag}</Tag>
         ))}
-      </p>
-      {content.trim() ? (
-        <>
-          <Pre>{addLinks(content)}</Pre>
-          <hr />
-        </>
-      ) : null}
-      <div>
-        <p>
-          Posted by{" "}
-          <Link href={getAuthorLink(author.id)}>
-            <strong>{author.name}</strong>
-          </Link>{" "}
-          on{" "}
-          <Link href={getJobLink(id)}>
-            {moment(date).format("MMMM Do YYYY")}
-          </Link>
-        </p>
-        {onClickGetInTouch ? (
-          <GetInTouch>
-            <button onClick={onClickGetInTouch}>How can I get in touch?</button>
-          </GetInTouch>
-        ) : null}
-      </div>
+      </Tags>
+      <PostBody dangerouslySetInnerHTML={{ __html: description }} />
+      {reactions.length > 0 && (
+        <Reactions>
+          {reactions.map(([e, c]) => (
+            <Reaction key={e}>
+              {e} {c}
+            </Reaction>
+          ))}
+        </Reactions>
+      )}
     </Wrapper>
   );
 };
--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -1,43 +1,42 @@
 import React from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 import { DiscordAuth } from "@components/DiscordAuth";
 
 import { Layout, Link, Modal } from "@components";
 import JobSearch from "../components/JobSearch";
 
+const queryClient = new QueryClient();
+
 const Jobs = () => {
   const [showModal, setModal] = React.useState(false);
   const toggleModal = () => setModal((prev) => !prev);
 
   return (
-    <Layout title="Jobs" largeTitle sidebar>
-      {(setSidebar) => (
-        <>
-          <h1>
-            Reactiflux <span>Jobs</span>
-          </h1>
-          <DiscordAuth>
-            <JobSearch toggleModal={toggleModal} setSidebar={setSidebar} />
-          </DiscordAuth>
-          <Modal close={() => setModal(false)} isOpen={showModal}>
-            <p>
-              If the job post does not contain a dedicated email, link, or phone
-              number, you can click either the offer date to open the original
-              message in our{" "}
-              <Link href="https://discord.gg/R942bNb">
-                <strong>#job-board</strong> channel
-              </Link>
-              , or the name of the user that posted the job. You can then
-              contact the person by sending them a direct message.
-            </p>
-            <p>
-              If you don’t already have one, you will need to create a (free!){" "}
-              <Link href="https://discord.com/">Discord</Link> account.
-            </p>
-          </Modal>
-        </>
-      )}
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout title="Jobs" largeTitle sidebar>
+        {(setSidebar) => (
+          <>
+            <h1>
+              Reactiflux <span>Jobs</span>
+            </h1>
+            <DiscordAuth>
+              <JobSearch toggleModal={toggleModal} setSidebar={setSidebar} />
+            </DiscordAuth>
+            <Modal close={() => setModal(false)} isOpen={showModal}>
+              <h4>How do I apply?</h4>
+              <p>
+                If the job post does not contain a dedicated email, link, or
+                phone number, you can click either the offer date to open the
+                original message in our <code>#job-board</code> channel , or the
+                name of the user that posted the job. You can then contact the
+                person by sending them a direct message.
+              </p>
+            </Modal>
+          </>
+        )}
+      </Layout>
+    </QueryClientProvider>
   );
 };
 
